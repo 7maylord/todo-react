601a50bbd6568b7a6acca6868b16172ddc0fa0f3
Olumide Adenigba <Olumideadenigba@gmail.com>
2024-09-18 21:48:40 +0100

    chore: added events & hooks

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,39 +1,38 @@
 import { useState } from "react";
 
-
-
 function Form(props) {
-
-    const [name, setName] = useState("");
-    function handleChange() {
-        console.log("Typing!");
-      }
-    function handleSubmit(event) {
-        event.preventDefault();
-        props.addTask("Todo Added!");
-      }
-      
-    return (
-      <form onSubmit={handleSubmit}>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-          value={name}
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
-    );
+  const [name, setName] = useState("");
+  function handleChange(event) {
+    setName(event.target.value);
   }
-  
-  export default Form;
-  
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.addTask(name);
+  setName("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          What needs to be done?
+        </label>
+      </h2>
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+        value={name}
+        onChange={handleChange}
+      />
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+    </form>
+  );
+}
+
+export default Form;
