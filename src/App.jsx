2ad26726106abecbe431ed93df005dfbd2602094
Olumide Adenigba <Olumideadenigba@gmail.com>
2024-09-18 22:34:40 +0100

    chore: added function to handle task completion

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,38 @@ function App(props) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
     setTasks([...tasks, newTask]);
   }
+  //Here, we define an updatedTasks constant that maps over the original tasks array. If the task's id property matches the id provided to the function, we use object spread syntax to create a new object, and toggle the completed property of that object before returning it. If it doesn't match, we return the original object.Then we call setTasks() with this new array in order to update our state.
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      console.log(tasks[0]);
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  function deleteTask(id) {
+    console.log(id);
+  }
+
   const taskList = tasks?.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
-  ))
+  ));
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
-  ;
   return (
     <div className="todoapp stack-large">
       <h1>Todo-App</h1>
